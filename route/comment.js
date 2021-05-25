8030271d14226973a501fd5b526dd27067c5dae2
Aditya Jain - Developer Indore <aadityajain.dev@protonmail.com>
2021-05-25 19:28:07 +0530

    today's date

--- route/comment.js
+++ route/comment.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const Comment = require('../models/Comment');
 const { Op } = require('sequelize');
 
+let todays_date = () => {
+    today = new Date();
+    let dd = today.getDate();
+
+    let mm = today.getMonth() + 1;
+    const yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = `0${dd}`;
+    }
+
+    if (mm < 10) {
+        mm = `0${mm}`;
+    }
+
+    return `${dd}-${mm}-${yyyy}`;
+};
+
 router.get('/:comment_id?', function (req, res) {
     if (!req.params.comment_id) {
         // send all comments for dashboard  code here
