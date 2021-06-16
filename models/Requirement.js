ac3a0b94535b515d60ee304b7c4bba4bbcf72ef0
Aditya Jain - Developer Indore <aadityajain.dev@protonmail.com>
2021-06-16 20:23:19 +0530

    new model according to requested changes

---
+++ models/Requirement.js
@@ -0,0 +1,71 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../database');
+const Employee = require('./Employee');
+const Client = require('./Client');
+
+const Requirement = db.define(
+    'Requirement',
+    {
+        requirement_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        emp_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Employee,
+                key: 'emp_id',
+            },
+        },
+        status: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        timeline: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        additional_note: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        skill_set: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        experience: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        number_of_position: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        updated_on: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        created_on: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        client_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Client,
+                key: 'client_id',
+            },
+        },
+    },
+    {
+        tableName: 'infobeans_requirements',
+        timestamps: false,
+    }
+);
+
+module.exports = Requirement;
